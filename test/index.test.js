58686c6f24ed42ed835ceb96c11ee7d0cc31f84c
UmeshGayashan <umeshgayashan099@gmail.com>
2024-04-05 22:46:30 +0530

    Basic Testing

--- test/index.test.js
+++ test/index.test.js
@@ -9,6 +9,20 @@ test('toEqual',()=>{
     expect(add(1,2)).toEqual(3)
 })
 
+// Check whether output undefined (Defined)
 test('toEqual',()=>{
     expect(add(1,2)).toBeDefined()
+})
+
+// Not to Be null
+test('toBeNull',()=>{
+    expect(add(1,2)).not.toBeNull()
+})
+
+test('toBeGreaterThan',()=>{
+    expect(add(1,2)).toBeGreaterThan(1)
+})
+
+test('toBeLessThan',()=>{
+    expect(add(1,2)).toBeLessThan(10)
 })
